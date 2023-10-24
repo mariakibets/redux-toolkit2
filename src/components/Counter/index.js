daac29281b715771ca68a25b95071f0325faf11b
Mari Kibets <mariakibec7@Gmail.com>
2023-10-24 14:11:38 +0200

    Add counter component

---
+++ src/components/Counter/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Counter = () => {
+  return (
+
+    <div>
+      <p> count: {1} </p>
+      <p>Step: {2} </p>
+      <button>Increment</button>
+      <button>Decrement</button>
+    </div>
+
+  );
+}
+
+export default Counter;
