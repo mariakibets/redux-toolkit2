de6fae67d740c94c704d8470098f801c8fe28427
Mari Kibets <mariakibec7@Gmail.com>
2023-10-26 16:49:01 +0200

    Add libs for styles + Header skeleton

---
+++ src/components/Header/index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+   <header>
+    <h1>My Site</h1>
+        <nav>
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>31</li>
+            </ul>
+        </nav>
+        <div>
+            <button>Switch theme</button>
+        </div>
+   </header>
+  )
+}
+
+export default Header;
