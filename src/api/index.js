c2a2f4031b444bed9cda7933eb2a2b4ee0ffdb81
Mari Kibets <mariakibec7@Gmail.com>
2023-10-29 13:33:22 +0100

    Add thunk error

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,4 @@
-export async function getUsers() {
-   const response = await fetch('https://jsonplaceholder.typicode.com/users');
-   const users = response.json();
-   return users;
-}
+import axios from "axios";
+
+export const getUsers = async () => await axios.get('https://jsonplaceholder.typicode.com/user');
+   
--- src/store/slices/userSlice.js
+++ src/store/slices/userSlice.js
@@ -3,8 +3,18 @@ import * as API from '../../api';
 
 const SLICE_NAME = 'users';
 
-const getUsers = createAsyncThunk(`${SLICE_NAME}/getUssers`, () =>{
-   return API.getUsers()
+const getUsers = createAsyncThunk(`${SLICE_NAME}/getUssers`, async (arg, thunkAPI) => {
+    
+    try{
+        console.log(arg)
+        const {data: users} =  await API.getUsers();
+
+        return users;
+    }catch (error){
+        console.log(error);
+
+        return thunkAPI.rejectWithValue(error.message);
+    }
 })
 
 const initialState = {
@@ -24,6 +34,11 @@ const userSlice = createSlice({
         builder.addCase(getUsers.fulfilled, (state, action) => {
             state.isLoading = false;
             state.users = action.payload;
+        });
+
+        builder.addCase(getUsers.rejected, (state, action) => {
+            state.isLoading = false;
+            state.error = action.payload;
         })
     },
         
